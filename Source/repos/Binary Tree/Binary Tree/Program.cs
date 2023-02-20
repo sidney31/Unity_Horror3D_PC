e0c5ee82fec97eec7172569184356d8afa37d72a
Суров Богдан <sidney31>
2023-02-20 09:35:34 +0300

    initial commit

---
+++ Source/repos/Binary Tree/Binary Tree/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        TreeNode fifth = new TreeNode(5);
+        TreeNode fourth = new TreeNode(4);
+        TreeNode third = new TreeNode(3);
+        TreeNode second = new TreeNode(1, fourth, fifth);
+        TreeNode first = new TreeNode(2, second, third);
+
+        InvertTree(first);
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static TreeNode InvertTree(TreeNode root)
+    {
+        Console.WriteLine(root.left.GetType);
+        (root.left, root.right) = (root.right, root.left);
+        return root;
+    }
+
+}
